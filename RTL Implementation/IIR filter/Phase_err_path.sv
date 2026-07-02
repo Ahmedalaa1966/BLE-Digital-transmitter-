e3688593cc1c675a2cc1494bc0ab2e3b0a580d0b
habibaae <167820600+habibaae@users.noreply.github.com>
2026-07-02 15:46:47 +0300

    Update Phase_err_path.sv

--- RTL Implementation/IIR filter/Phase_err_path.sv
+++ RTL Implementation/IIR filter/Phase_err_path.sv
@@ -1,6 +1,6 @@
 module phase_err_path # (
    parameter INPUT_WIDTH_1 = 40,        
-   parameter INPUT_WIDTH_2 = 43,        //24 int , 16 fractional
+   parameter INPUT_WIDTH_2 = 43,     
    
    parameter OUT_WIDTH = 11
 ) ( 
@@ -20,8 +20,7 @@ logic signed [INPUT_WIDTH_2+7-1:0] o_phase_err_iir1_ext;
 
 //---------------------- Instantiate DUT---------------------------//
 IIR_1 #(
-    .INPUT_WIDTH(INPUT_WIDTH_1),
-    .ALPHA_IIR(3)                        // alpha_iir = 2^(-4)
+    .INPUT_WIDTH(INPUT_WIDTH_1)
 ) IIR_1 (
     .i_clk(i_clk),
     .i_rst_n(i_rst_n),
@@ -31,8 +30,7 @@ IIR_1 #(
 );
 
 IIR_2 #(
-    .INPUT_WIDTH(INPUT_WIDTH_2),
-    .ALPHA_IIR(6)                        // alpha_iir = 2^(-6)
+    .INPUT_WIDTH(INPUT_WIDTH_2)
 ) IIR_2 (
     .i_clk(i_clk),
     .i_rst_n(i_rst_n),
@@ -41,23 +39,10 @@ IIR_2 #(
     .o_iir(o_phase_err_iir2)
 );
 
-/*
-always @(posedge i_clk) begin
-
-o_err_full <= o_phase_err_iir1_ext - signed'({o_phase_err_iir2[INPUT_WIDTH_2+6-1], o_phase_err_iir2});
-//o_err <= o_err_full[INPUT_WIDTH-1: 10];
-
-end
-*/
-
 assign o_phase_err_iir1_ext = signed'({o_phase_err_iir1[INPUT_WIDTH_2-1], o_phase_err_iir1, 6'd0});
 
 assign o_err_full = o_phase_err_iir1_ext - signed'({o_phase_err_iir2[INPUT_WIDTH_2+6-1], o_phase_err_iir2});
 
 assign o_err = o_err_full[25:15];
-//if full was 21 and we need 11
-//assign o_err = o_err_full[25:15];
-
-
 
 endmodule
